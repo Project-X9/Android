6f7b6fe9257c2a7b50999a17e75a870b12ab8f78
Samy Bahaa <samyking.sb@gmail.com>
2020-03-16 19:07:17 +0200

    make the front end of the empty playlist and added a playlist that contain some dummy songs to show how it look

---
+++ ProjectX/app/src/main/java/com/example/projectx/SongAdapter.java
@@ -0,0 +1,56 @@
+package com.example.projectx;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
+    private ArrayList<SongList> mSongItems;
+
+    public SongAdapter(ArrayList<SongList> SongItems) {
+        mSongItems=SongItems;
+    }
+
+    public static class SongViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public SongViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.song_iv);
+            mTextView1 = itemView.findViewById(R.id.singer_tv);
+            mTextView2 = itemView.findViewById(R.id.song_name_tv);
+        }
+    }
+
+    @NonNull
+    @Override
+    public SongViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_song_list,parent,false);
+        SongViewHolder evh = new SongViewHolder(v);
+        return  evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongViewHolder holder, int position) {
+        SongList currentItem = mSongItems.get(position);
+        holder.mImageView.setImageResource(currentItem.getmImageResoure());
+        holder.mTextView1.setText(currentItem.getmText1());
+        holder.mTextView2.setText(currentItem.getmText2());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSongItems.size();
+    }
+}
